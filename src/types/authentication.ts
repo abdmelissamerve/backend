467cb600bfe9ebec88fe9dddcd9d45ca723b7a13
abdmelissamerve <90768816+abdmelissamerve@users.noreply.github.com>
2023-05-06 16:43:18 +0300

    validations added

--- src/types/authentication.ts
+++ src/types/authentication.ts
@@ -9,9 +9,22 @@ export interface RegisterInputParams {
 }
 
 export const registerSchema = Joi.object({
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
+    firstName: Joi.string()
+        .min(2)
+        .max(50)
+        .pattern(/^[a-zA-Z\s]+$/, { name: "letters and spaces" })
+        .required(),
+    lastName: Joi.string()
+        .min(2)
+        .max(50)
+        .pattern(/^[a-zA-Z\s]+$/, { name: "letters and spaces" })
+        .required(),
+
     email: Joi.string().email().required(),
-    password: Joi.string().required(),
-    phoneNumber: Joi.string().required(),
+    password: Joi.string().min(8).required(),
+    phoneNumber: Joi.string()
+        .pattern(/^0((\([0-9]{2,3}\))|([0-9]{1,3}))*?[0-9]{3,4}?[0-9]{3,4}?$/, { name: "valid phone number" })
+        .min(10)
+        .max(10)
+        .required(),
 });
